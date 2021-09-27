1eb539b64017b4e7d13c46e9872d4cd532368b3b
Bole <footgod368@sjtu.edu.cn>
2021-09-27 13:17:49 +0800

    Makefile

---
+++ CPP/multiple_files/add.cpp
@@ -0,0 +1,6 @@
+int add(int a, int b)
+{
+    return (a+b);
+}
+//for function definition, there's no need to #include the ".h" file
+//However, for class member function definition, ".h" is needed
--- test_codes/test.cpp
+++ test_codes/test.cpp
@@ -1,1 +1,4 @@
 //for test
+#include <iostream>
+
+using namespace std;
