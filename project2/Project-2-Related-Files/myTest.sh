48e4737482b6bff345d3a506754df3826dc3a3d7
Bole <footgod368@sjtu.edu.cn>
2021-10-21 15:14:16 +0800

    1

--- project2/Project-2-Related-Files/myTest.sh
+++ project2/Project-2-Related-Files/myTest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-g++ -c myTest.cpp
-g++ -c p2.cpp
-g++ -c recursive.cpp
+g++ -c myTest.cpp -Wall
+g++ -c p2.cpp -Wall
+g++ -c recursive.cpp -Wall
 g++ -o myTest p2.o recursive.o myTest.o
 
 ./myTest
